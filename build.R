073cf102c200e23b21f0060eec2685b4673d20ef
Michael DeWitt <michael.dewitt@conehealth.com>
2020-07-13 06:53:23 -0400

    for ga a

--- build.R
+++ build.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/54011/Documents/pandemic/covid-data")
+#setwd("C:/Users/54011/Documents/pandemic/covid-data")
 src_files <-  fs::dir_ls(path = here::here("src"), glob = "*.R")
 
 lapply(X = src_files, try(expr = source))
