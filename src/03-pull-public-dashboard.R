dff7fc30eba65c08d1f79f320bda38cddafc7853
Michael DeWitt <michael.dewitt@conehealth.com>
2020-07-13 07:20:56 -0400

    fix some things

--- src/03-pull-public-dashboard.R
+++ src/03-pull-public-dashboard.R
@@ -1,44 +1,45 @@
-url <- "https://public.tableau.com/vizql/w/NCDHHS_COVID-19_DataDownload/v/DailyMetrics/vudcsv/sessions/60CEC09CA2284E74A01A2CE6903BA11F-0:0/views/4771422506905449664_9024048749190726675?showall=true&underlying_table_id=Migrated%20Data&underlying_table_caption=Full%20Data"
+url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/DailyMetrics.csv?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=no&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=no&publish=yes&:loadOrderID=0"
 current_time <- format(Sys.time(), "%Y%m%d%H%M")
 
 try(download.file(url = url, destfile = here::here("data", "daily-metrics",
                                                paste0(current_time,"_ncdhss_metrics.csv"))))
 
 
+
 # age ---------------------------------------------------------------------
 
-url <- "https://public.tableau.com/vizql/w/NCDHHS_COVID-19_DataDownload/v/Demographics/vudcsv/sessions/7D331135251941A49478767178E2F38C-0:0/views/5649504231100340473_6474227137836628765?showall=true&underlying_table_id=Migrated%20Data&underlying_table_caption=Full%20Data"
+url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/Demographics.csv?%3Aembed=y&%3AshowVizHome=no&%3Ahost_url=https%3A%2F%2Fpublic.tableau.com%2F&%3Aembed_code_version=3&%3Aembed_code_version=3&%3Atabs=yes&%3Atabs=yes&%3Atoolbar=no&%3Atoolbar=no&%3Aanimate_transition=yes&%3Aanimate_transition=yes&%3Adisplay_static_image=no&%3Adisplay_static_image=no&%3Adisplay_spinner=no&%3Adisplay_spinner=no&%3Adisplay_overlay=yes&%3Adisplay_overlay=yes&%3Adisplay_count=no&%3Adisplay_count=no&publish=yes&publish=yes&%3AloadOrderID=0u.com%2F&%3AloadOrderID=0"
 
 try(download.file(url = url, destfile = here::here("data", "daily-age",
                                                    paste0(current_time,"_ncdhss_age.csv"))))
 
 
-# race --------------------------------------------------------------------
-
-url <- "https://public.tableau.com/vizql/w/NCDHHS_COVID-19_DataDownload/v/Demographics/vudcsv/sessions/7D331135251941A49478767178E2F38C-0:0/views/5649504231100340473_15757585069639442359?showall=true&underlying_table_id=Migrated%20Data&underlying_table_caption=Full%20Data"
-
-try(download.file(url = url, destfile = here::here("data", "daily-race",
-                                                   paste0(current_time,"_ncdhss_race.csv"))))
-
-# ethnicity ---------------------------------------------------------------
-
-url <- "https://public.tableau.com/vizql/w/NCDHHS_COVID-19_DataDownload/v/Demographics/vudcsv/sessions/7D331135251941A49478767178E2F38C-0:0/views/5649504231100340473_5745676187720193715?showall=true&underlying_table_id=Migrated%20Data&underlying_table_caption=Full%20Data"
-
-try(download.file(url = url, destfile = here::here("data", "daily-eth",
-                                                   paste0(current_time,"_ncdhss_eth.csv"))))
-
-
-# gender ------------------------------------------------------------------
-
-url <- "https://public.tableau.com/vizql/w/NCDHHS_COVID-19_DataDownload/v/Demographics/vudcsv/sessions/7D331135251941A49478767178E2F38C-0:0/views/5649504231100340473_14942902393317967217?showall=true&underlying_table_id=Migrated%20Data&underlying_table_caption=Full%20Data"
-
-try(download.file(url = url, destfile = here::here("data", "daily-sex",
-                                                   paste0(current_time,"_ncdhss_sex.csv"))))
+# # race --------------------------------------------------------------------
+#
+# url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/Demographics.csv?Table=TABLE_ETHNICITY%3Aembed=y&%3AshowVizHome=no&%3Ahost_url=https%3A%2F%2Fpublic.tableau.com%2F&%3Aembed_code_version=3&%3Aembed_code_version=3&%3Atabs=yes&%3Atabs=yes&%3Atoolbar=no&%3Atoolbar=no&%3Aanimate_transition=yes&%3Aanimate_transition=yes&%3Adisplay_static_image=no&%3Adisplay_static_image=no&%3Adisplay_spinner=no&%3Adisplay_spinner=no&%3Adisplay_overlay=yes&%3Adisplay_overlay=yes&%3Adisplay_count=no&%3Adisplay_count=no&publish=yes&publish=yes&%3AloadOrderID=0u.com%2F&%3AloadOrderID=0&viz=%7B%22worksheet%22%3A%22TABLE_ETHNICITY"
+#
+# try(download.file(url = url, destfile = here::here("data", "daily-race",
+#                                                    paste0(current_time,"_ncdhss_race.csv"))))
+#
+# # ethnicity ---------------------------------------------------------------
+#
+# url <- "https://public.tableau.com/vizql/w/NCDHHS_COVID-19_DataDownload/v/Demographics/vudcsv/sessions/7D331135251941A49478767178E2F38C-0:0/views/5649504231100340473_5745676187720193715?showall=true&underlying_table_id=Migrated%20Data&underlying_table_caption=Full%20Data"
+#
+# try(download.file(url = url, destfile = here::here("data", "daily-eth",
+#                                                    paste0(current_time,"_ncdhss_eth.csv"))))
+#
+#
+# # gender ------------------------------------------------------------------
+#
+# url <- "https://public.tableau.com/vizql/w/NCDHHS_COVID-19_DataDownload/v/Demographics/vudcsv/sessions/7D331135251941A49478767178E2F38C-0:0/views/5649504231100340473_14942902393317967217?showall=true&underlying_table_id=Migrated%20Data&underlying_table_caption=Full%20Data"
+#
+# try(download.file(url = url, destfile = here::here("data", "daily-sex",
+#                                                    paste0(current_time,"_ncdhss_sex.csv"))))
 
 
 # beds and vents ----------------------------------------------------------
 
-url <- "https://public.tableau.com/vizql/w/NCDHHS_COVID-19_DataDownload/v/HospitalBeds/vudcsv/sessions/7D331135251941A49478767178E2F38C-0:0/views/1140134203458450422_7087943268751700638?summary=true"
+url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/HospitalBeds.csv?%3Aembed=y&%3AshowVizHome=no&%3Ahost_url=https%3A%2F%2Fpublic.tableau.com%2F&%3Aembed_code_version=3&%3Aembed_code_version=3&%3Atabs=yes&%3Atabs=yes&%3Atoolbar=no&%3Atoolbar=no&%3Aanimate_transition=yes&%3Aanimate_transition=yes&%3Adisplay_static_image=no&%3Adisplay_static_image=no&%3Adisplay_spinner=no&%3Adisplay_spinner=no&%3Adisplay_overlay=yes&%3Adisplay_overlay=yes&%3Adisplay_count=no&%3Adisplay_count=no&publish=yes&publish=yes&%3AloadOrderID=0u.com%2F&%3AloadOrderID=0"
 
 try(download.file(url = url, destfile = here::here("data", "daily-beds",
                                                    paste0(current_time,"_ncdhss_beds.csv"))))
