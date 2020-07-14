3927ee7988f11b749eecd5a2926bf417794e7e4e
Michael DeWitt <michael.dewitt@conehealth.com>
2020-07-14 10:26:32 -0400

    shell script that will parse demographic data. still need to work into combining all pds, but this will do most things

--- src/04-clean-pdfs.sh
+++ src/04-clean-pdfs.sh
@@ -1,6 +1,11 @@
 #! bin/bash
 
-sed 's/  */ /g' test.txt> test2.txt
-sed 's/^$/d/g' test2.txt
+sed 's/    */\t/g' test.txt> test2.txt
 
-awk '{print $1,$2, $3, $4}' test2.txt
+awk '$1 ~ /Demographics|Race|Missing|Cases|Age|Gender/ {next} {print}' test2.txt | awk '$1 ~ /Pacific|Ethnicity|Alaska/ {next} {print}' > test3.txt
+
+awk 'NR==1{c=$0} !/^\t/{print c; c=$0} /^\t/{c=c ""$0} END{print c}' test3.txt | grep "[0-9]" > "demos_final.txt"
+
+awk '{print $0}' demos_final.txt
+
+rm test2.txt test3.txt
