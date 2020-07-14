d7d5a0ec7b9c1853153f61abdc3e42e4b98be085
DeWitt <michael.dewitt@conehealth.com>
2020-07-14 15:57:44 -0400

    'auto-update'

--- build-demos.R
+++ build-demos.R
@@ -1,5 +1,9 @@
 setwd("C:/Users/54011/Documents/pandemic/covid-data")
 
+cmd_cd <- glue::glue("git pull origin master")
+
+system(cmd_cd)
+
 try(source("src/04-clean-pdfs.R"))
 
 cmd_cd <- glue::glue("git add .")
