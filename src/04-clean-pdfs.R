ce48ffa3fd24192401a1a4d7b805859adb10cd86
Michael DeWitt <michael.dewitt@conehealth.com>
2020-07-14 18:54:04 -0400

    fixed maybe an issue with timezone conversion

--- src/04-clean-pdfs.R
+++ src/04-clean-pdfs.R
@@ -5,10 +5,18 @@ cat("Starting Pdf Scrapes\n\n")
 # bring in files and select oldest by date --------------------------------
 in_files <- fs::dir_info(file.path("data", "daily-race"), glob = "*.pdf")
 
+
+in_files$update_date <- gsub(pattern = "([0-9]+).*$", "\\1",basename(in_files$path))
+
+in_files$update_date <- as.POSIXct(in_files$update_date, format = "%Y%m%d%H%M")
+
+in_files$update_date_only <- lubridate::date(in_files$update_date)
+
 in_files <- in_files %>%
-  dplyr::mutate(date = lubridate::date(birth_time)) %>%
-  dplyr::group_by(date) %>%
-  dplyr::filter(birth_time==max(birth_time))
+  dplyr::group_by(update_date_only) %>%
+  dplyr::filter(update_date==max(update_date)) %>%
+  slice(1) %>%
+  ungroup()
 
 in_pdf <- lapply(in_files%>%
                    dplyr::pull(path), pdftools::pdf_text)
