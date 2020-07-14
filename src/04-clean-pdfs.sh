c592a49be367d82c4bf47d51ea1fa2f2f15f297e
Michael DeWitt <michael.dewitt@conehealth.com>
2020-07-14 13:17:25 -0400

    adding

--- src/04-clean-pdfs.sh
+++ src/04-clean-pdfs.sh
@@ -6,6 +6,6 @@ awk '$1 ~ /Demographics|Race|Missing|Cases|Age|Gender/ {next} {print}' test2.txt
 
 awk 'NR==1{c=$0} !/^\t/{print c; c=$0} /^\t/{c=c ""$0} END{print c}' test3.txt | grep "[0-9]" > "demos_final.txt"
 
-awk '{print $0}' demos_final.txt
+#awk '{print $0}' demos_final.txt
 
 rm test2.txt test3.txt
