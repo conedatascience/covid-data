e73da932cf822eada4a8702ed5816e3106b71dce
Michael DeWitt <michael.dewitt@conehealth.com>
2020-07-14 07:56:53 -0400

    adding new deps and commenting bad dev scripts

--- src/04-clean-pdfs.R
+++ src/04-clean-pdfs.R
@@ -1,8 +1,8 @@
-
-in_files <- fs::dir_ls(file.path("data", "daily-race"), glob = "*.pdf")
-
-in_pdf <- lapply(in_files, pdftools::pdf_text)
-
-writeLines(in_pdf[[1]], "test.txt")
-
-trimws(strsplit(readLines("test.txt")[24])
+#
+# in_files <- fs::dir_ls(file.path("data", "daily-race"), glob = "*.pdf")
+#
+# in_pdf <- lapply(in_files, pdftools::pdf_text)
+#
+# writeLines(in_pdf[[1]], "test.txt")
+#
+# trimws(strsplit(readLines("test.txt")[24])
