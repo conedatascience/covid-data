e97cf7e4ed9b0fe283ca503a10dbb19e98493c16
Michael DeWitt <michael.dewitt@conehealth.com>
2020-09-27 09:31:10 -0400

    fixing for the way that the state changed reporting method now including antigen tests and changing column names:

--- src/03-pull-public-dashboard.R
+++ src/03-pull-public-dashboard.R
@@ -1,9 +1,8 @@
-url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/DailyMetrics.csv?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=no&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=no&publish=yes&:loadOrderID=0"
+url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/DailyCasesandDeathsMetrics.csv?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=no&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=no&publish=yes&:loadOrderID=0"
 current_time <- format(Sys.time(), "%Y%m%d%H%M")
 
 try(download.file(url = url, destfile = here::here("data", "daily-metrics",
                                                paste0(current_time,"_ncdhss_metrics.csv"))))
-
 
 
 # age ---------------------------------------------------------------------
