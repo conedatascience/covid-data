9bfe49aa109e8a1894cbd3b54be9fb36d281e8af
Michael DeWitt <michael.dewitt@conehealth.com>
2021-01-11 08:17:36 -0500

    local stuff

--- src/08-compile-vaccines.R
+++ src/08-compile-vaccines.R
@@ -25,15 +25,36 @@ first_dist <- as.Date("2020-12-14")
 
 dat_raw[,county:= stringr::str_remove(string = county, "County")]
 
+report_date <- purrr::map_dfr(data_dir,
+                              ~readxl::read_excel(.x, range = "A1:A1", sheet = 2,
+                                                  col_names = c("date")),
+                              .id = "date_pulled"
+)
+
+setDT(report_date)
+
+report_date[, update_date := gsub(pattern = "([0-9]+).*$", "\\1",basename(date_pulled))]
+
+report_date[, update_date := as.POSIXct(update_date, format = "%Y%m%d%H%M")]
+
+report_date[,reported_date:=stringr::str_extract(date, pattern = "(\\D{3}\\. \\d{1}|\\d{2})")]
+
+report_date[,reported_date:=lubridate::mdy(paste0(reported_date, " 2021"))]
+
+report_date <- report_date[,c("reported_date", "date_pulled")]
+# join information together -----------------------------------------------
+
+dat_raw <- merge(dat_raw, report_date, by = "date_pulled", all.x = TRUE)
+
 # diffing -----------------------------------------------------------------
 
 dat_raw[ ,date:= lubridate::date(update_date)]
 
 dat_latest <- dat_raw[update_date==max(update_date), , by = "date"]
 
-dat_latest <- dcast(formula = date+county~vaccine_status, value.var = "total_doses", data = dat_latest)
+dat_latest <- dcast(formula = date+reported_date+county~vaccine_status, value.var = "total_doses", data = dat_latest)
 
-names(dat_latest) <- c("date", "county", "dose_1", "dose_2")
+names(dat_latest) <- c("date", "reported_date","county", "dose_1", "dose_2")
 
 dat_latest[order(date),`:=` (daily_dose_1 = dose_1 - shift(dose_1,1, fill = 0),
                   daily_dose_2 = dose_2 - shift(dose_2,1, fill = 0)), by = "county"]
