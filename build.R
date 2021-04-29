b2d1e0c003cba7d2d2644969403d8ca258d0a942
DS2 <michael.dewitt@conehealth.com>
2021-04-29 10:08:17 -0400

    Lastest update at 2021-04-29 10:08:17

--- build.R
+++ build.R
@@ -6,3 +6,7 @@ cmd <- glue::glue("Rscript --vanilla {src_files}")
 for(i in seq_along(cmd)){
     system(cmd[i])
 }
+
+system("git add --all")
+
+gert::git_commit(glue::glue("Lastest update at {Sys.time()}"))
