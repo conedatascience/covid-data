e78fa8cbc3690a60fd2784933eb45901b7deed38
DS2 <michael.dewitt@conehealth.com>
2021-05-05 14:08:39 +0000

    dont need to run the move cases from the scraper

--- src/move-cases.R
+++ src/move-cases.R
@@ -1,18 +1,18 @@
 # Fix Some Errors
 
-library(tidyverse)
+#library(tidyverse)
 
-target_files <- read_csv("file-mv.txt",     col_names = "target")
+#target_files <- read_csv("file-mv.txt",     col_names = "target")
 
-for(i in 1:nrow(target_files)){
-    target_path <- file.path("daily-scrape",target_files$target[i] )
+#for(i in 1:nrow(target_files)){
+#    target_path <- file.path("daily-scrape",target_files$target[i] )
 
-    dat_raw <- readr::read_csv(target_path)
+#    dat_raw <- readr::read_csv(target_path)
 
-    names(dat_raw) <- c("Deaths", "Casesper10k", "Casesper100k", "County", "Total")
+#    names(dat_raw) <- c("Deaths", "Casesper10k", "Casesper100k", "County", "Total")
 
-    dat_raw$PctPos <- NA
+#    dat_raw$PctPos <- NA
 
-  new_path <- file.path("covid-data", "data", "daily", target_files$target[i])
-    write_csv(dat_raw, new_path)
-}
+ # new_path <- file.path("covid-data", "data", "daily", target_files$target[i])
+  #  write_csv(dat_raw, new_path)
+#}
