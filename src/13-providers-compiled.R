da1378ceac55fe591a85726ccc3c2e36486b4965
DS2 <michael.dewitt@conehealth.com>
2021-07-07 17:03:48 +0000

    Lastest update at 2021-07-07 17:03:47

--- src/13-providers-compiled.R
+++ src/13-providers-compiled.R
@@ -6,7 +6,29 @@ data_dir <- fs::dir_ls(here::here("data", "daily-vax-providers"),  glob = "*csv"
 
 # import data -------------------------------------------------------------
 
-dat_raw <- purrr::map_dfr(data_dir, data.table::fread, .id = "date_pulled")
+
+read_valid <- function(x){
+    o <- data.table::fread(x)
+
+    rcnt <- nrow(o)
+
+    search_for_these <- c( "COVID_19VaccinationProviderType", "V1", "V2")
+    replace_with_these <- c("ProviderType", "X", "Y")
+    i1 <- match(colnames(o), search_for_these, nomatch = 0)
+    
+    colnames(o)[i1] <- replace_with_these[i1]
+
+    if(rcnt>1){
+        o
+    } else {
+        NULL
+    }
+
+}
+
+#dat_raw <- rbindlist(lapply(data_dir, data.table::fread),  idcol = "date_pulled", fill = TRUE)
+
+dat_raw <- rbindlist(lapply(data_dir,read_valid),  idcol = "date_pulled", fill = TRUE)
 
 setDT(dat_raw)
 
@@ -21,6 +43,12 @@ dat_raw[, update_date_date := lubridate::date(update_date)]
 dat_latest <- dat_raw[dat_raw[, .I[update_date == max(update_date)], by=c("OBJECTID","update_date_date")]$V1]
 
 dat_latest <- dat_latest[order(update_date_date)]
+
+dat_latest[,POINT_X:=NULL]
+dat_latest[,POINT_Y:=NULL]
+dat_latest[,V1:=NULL]
+dat_latest[,V2:=NULL]
+dat_latest[,SysKey:=NULL]
 
 # Write Outputs
 
