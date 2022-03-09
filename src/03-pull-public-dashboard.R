b6f6ff2eee6c00db686241673977c171a5e18272
Jennifer Wenner <jennifer.wenner@conehealth.com>
2022-03-09 19:38:31 +0000

    auto

--- src/03-pull-public-dashboard.R
+++ src/03-pull-public-dashboard.R
@@ -1,29 +1,29 @@
-url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/DailyCasesandDeathsMetrics.csv?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=no&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=no&publish=yes&:loadOrderID=0"
+#url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/DailyCasesandDeathsMetrics.csv?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=no&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=no&publish=yes&:loadOrderID=0"
 current_time <- format(Sys.time(), "%Y%m%d%H%M")
-try(download.file(url = url, destfile = here::here("data", "daily-metrics",
-                                               paste0(current_time,"_ncdhss_metrics.csv"))))
+#try(download.file(url = url, destfile = here::here("data", "daily-metrics",
+ #                                              paste0(current_time,"_ncdhss_metrics.csv"))))
 # Testing
-url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/DailyTestingMetrics.csv?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=no&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=no&publish=yes&:loadOrderID=0&:worksheet=TABLE_DAILY_TESTING_METRICS"
+#url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/DailyTestingMetrics.csv?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=no&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=no&publish=yes&:loadOrderID=0&:worksheet=TABLE_DAILY_TESTING_METRICS"
 #url<- "https://public.tableau.com/vizql/w/NCDHHS_COVID-19_DataDownload/v/DailyTestingMetrics/vudcsv/sessions/08A22C7CF5DF404B9AA08FB6B780AA0F-0:0/views/18079243224349496428_12928853633925886214?underlying_table_id=Migrated%20Data&underlying_table_caption=Full%20Data"
-try(metrics <-httr::content(httr::GET(url)))
+#try(metrics <-httr::content(httr::GET(url)))
 
-try(data.table::fwrite(metrics, here::here("data", "daily-testing",
-                                                   paste0(current_time,"_ncdhss_metrics.csv"))))
+#try(data.table::fwrite(metrics, here::here("data", "daily-testing",
+ #                                                  paste0(current_time,"_ncdhss_metrics.csv"))))
 # age ---------------------------------------------------------------------
 
-url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/Demographics.csv?%3Aembed=y&%3AshowVizHome=no&%3Ahost_url=https%3A%2F%2Fpublic.tableau.com%2F&%3Aembed_code_version=3&%3Aembed_code_version=3&%3Atabs=yes&%3Atabs=yes&%3Atoolbar=no&%3Atoolbar=no&%3Aanimate_transition=yes&%3Aanimate_transition=yes&%3Adisplay_static_image=no&%3Adisplay_static_image=no&%3Adisplay_spinner=no&%3Adisplay_spinner=no&%3Adisplay_overlay=yes&%3Adisplay_overlay=yes&%3Adisplay_count=no&%3Adisplay_count=no&publish=yes&publish=yes&%3AloadOrderID=0u.com%2F&%3AloadOrderID=0"
+#url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/Demographics.csv?%3Aembed=y&%3AshowVizHome=no&%3Ahost_url=https%3A%2F%2Fpublic.tableau.com%2F&%3Aembed_code_version=3&%3Aembed_code_version=3&%3Atabs=yes&%3Atabs=yes&%3Atoolbar=no&%3Atoolbar=no&%3Aanimate_transition=yes&%3Aanimate_transition=yes&%3Adisplay_static_image=no&%3Adisplay_static_image=no&%3Adisplay_spinner=no&%3Adisplay_spinner=no&%3Adisplay_overlay=yes&%3Adisplay_overlay=yes&%3Adisplay_count=no&%3Adisplay_count=no&publish=yes&publish=yes&%3AloadOrderID=0u.com%2F&%3AloadOrderID=0"
 
-try(download.file(url = url, destfile = here::here("data", "daily-age",
-                                                   paste0(current_time,"_ncdhss_age.csv"))))
+#try(download.file(url = url, destfile = here::here("data", "daily-age",
+ #                                                  paste0(current_time,"_ncdhss_age.csv"))))
 
 
 # # race --------------------------------------------------------------------
 #
-url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/Demographics.pdf?%3Aembed=y&%3AshowVizHome=no&%3Ahost_url=https%3A%2F%2Fpublic.tableau.com%2F&%3Aembed_code_version=3&%3Aembed_code_version=3&%3Atabs=yes&%3Atabs=yes&%3Atoolbar=no&%3Atoolbar=no&%3Aanimate_transition=yes&%3Aanimate_transition=yes&%3Adisplay_static_image=no&%3Adisplay_static_image=no&%3Adisplay_spinner=no&%3Adisplay_spinner=no&%3Adisplay_overlay=yes&%3Adisplay_overlay=yes&%3Adisplay_count=no&%3Adisplay_count=no&publish=yes&publish=yes&%3AloadOrderID=0u.com%2F&%3AloadOrderID=0"
+#url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/Demographics.pdf?%3Aembed=y&%3AshowVizHome=no&%3Ahost_url=https%3A%2F%2Fpublic.tableau.com%2F&%3Aembed_code_version=3&%3Aembed_code_version=3&%3Atabs=yes&%3Atabs=yes&%3Atoolbar=no&%3Atoolbar=no&%3Aanimate_transition=yes&%3Aanimate_transition=yes&%3Adisplay_static_image=no&%3Adisplay_static_image=no&%3Adisplay_spinner=no&%3Adisplay_spinner=no&%3Adisplay_overlay=yes&%3Adisplay_overlay=yes&%3Adisplay_count=no&%3Adisplay_count=no&publish=yes&publish=yes&%3AloadOrderID=0u.com%2F&%3AloadOrderID=0"
 
-a <- rvest::html_session(url)
-writeBin(a$response$content,here::here("data", "daily-race",
-                                       paste0(current_time,"_ncdhss_race.pdf")))
+#a <- rvest::html_session(url)
+#writeBin(a$response$content,here::here("data", "daily-race",
+ #                                      paste0(current_time,"_ncdhss_race.pdf")))
 # # ethnicity ---------------------------------------------------------------
 #
 # url <- "https://public.tableau.com/vizql/w/NCDHHS_COVID-19_DataDownload/v/Demographics/vudcsv/sessions/7D331135251941A49478767178E2F38C-0:0/views/5649504231100340473_5745676187720193715?showall=true&underlying_table_id=Migrated%20Data&underlying_table_caption=Full%20Data"
@@ -42,10 +42,10 @@ writeBin(a$response$content,here::here("data", "daily-race",
 
 # beds and vents ----------------------------------------------------------
 
-url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/HospitalBeds.csv?%3Aembed=y&%3AshowVizHome=no&%3Ahost_url=https%3A%2F%2Fpublic.tableau.com%2F&%3Aembed_code_version=3&%3Aembed_code_version=3&%3Atabs=yes&%3Atabs=yes&%3Atoolbar=no&%3Atoolbar=no&%3Aanimate_transition=yes&%3Aanimate_transition=yes&%3Adisplay_static_image=no&%3Adisplay_static_image=no&%3Adisplay_spinner=no&%3Adisplay_spinner=no&%3Adisplay_overlay=yes&%3Adisplay_overlay=yes&%3Adisplay_count=no&%3Adisplay_count=no&publish=yes&publish=yes&%3AloadOrderID=0u.com%2F&%3AloadOrderID=0"
+#url <- "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/HospitalBeds.csv?%3Aembed=y&%3AshowVizHome=no&%3Ahost_url=https%3A%2F%2Fpublic.tableau.com%2F&%3Aembed_code_version=3&%3Aembed_code_version=3&%3Atabs=yes&%3Atabs=yes&%3Atoolbar=no&%3Atoolbar=no&%3Aanimate_transition=yes&%3Aanimate_transition=yes&%3Adisplay_static_image=no&%3Adisplay_static_image=no&%3Adisplay_spinner=no&%3Adisplay_spinner=no&%3Adisplay_overlay=yes&%3Adisplay_overlay=yes&%3Adisplay_count=no&%3Adisplay_count=no&publish=yes&publish=yes&%3AloadOrderID=0u.com%2F&%3AloadOrderID=0"
 
-try(download.file(url = url, destfile = here::here("data", "daily-beds",
-                                                   paste0(current_time,"_ncdhss_beds.csv"))))
+#try(download.file(url = url, destfile = here::here("data", "daily-beds",
+ #                                                  paste0(current_time,"_ncdhss_beds.csv"))))
 
 
 # outbreaks ---------------------------------------------------------------
