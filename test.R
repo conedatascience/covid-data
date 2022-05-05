0f9e8c53ce04132bbeaeecc9dfdd310fa29ed552
Jennifer Wenner <jennifer.wenner@conehealth.com>
2022-05-05 18:19:51 +0000

    Lastest update at 2022-05-05 18:19:50

--- test.R
+++ test.R
@@ -2,7 +2,4 @@
 src_files <-  fs::dir_ls(path = here::here("src"), glob = "*.R")
 
 print(Sys.getenv('edw_dsn_default'))
-print(Sys.getenv('EDW_DSN_DEFAULT'))
-print(Sys.getenv('R_HOME'))
-print(Sys.getenv('R_USER'))
 
