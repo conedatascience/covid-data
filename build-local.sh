58aede11713796dbafbd0b9b4c0e71171e796514
Jason Muhlenkamp <jason.muhlenkamp@conehealth.com>
2022-06-17 14:47:09 +0000

    Add set -e; change R CMD to Rscript

--- build-local.sh
+++ build-local.sh
@@ -1,8 +1,10 @@
+set -e
+
 cd /datadisk/covid-data-store/covid-data
 
 git pull origin master
 
-R CMD BATCH --vanilla build.R
+Rscript --vanilla build.R
 
 git add .
 git commit -m "Update the latest data" -a || echo "No changes to commit"
