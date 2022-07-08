021fdd933474470fb98f8ae75756c0e46e8c5723
Jason Muhlenkamp <jason.muhlenkamp@conehealth.com>
2022-07-08 14:28:16 +0000

    Lastest update at 2022-07-08 14:28:15

--- build.R
+++ build.R
@@ -4,6 +4,7 @@ src_files <-  fs::dir_ls(path = here::here("src"), glob = "*.R")
 cmd <- glue::glue("Rscript --vanilla {src_files}")
 
 for(i in seq_along(cmd)){
+    print(cmd[i])
     system(cmd[i])
 }
 
