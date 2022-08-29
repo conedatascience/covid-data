d125cbdabedc1a2065bf5d45e45b34a6127a7a5a
Jennifer Wenner <jennifer.wenner@conehealth.com>
2022-08-29 16:59:37 +0000

    Lastest update at 2022-08-29 16:59:34

--- build.R
+++ build.R
@@ -10,5 +10,7 @@ for(i in seq_along(cmd)){
 
 system("git add --all")
 msg = glue::glue("Lastest update at {Sys.time()}")
-gert::git_commit(  msg)
-gert::git_push()
+# gert::git_commit(  msg)
+system(glue::glue('git commit -m "{msg}"'))
+# gert::git_push()
+system('git push')
