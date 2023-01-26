3fa13407b16bda26c0f8677b460f17185c1eeeef
Jennifer Wenner <jennifer.wenner@conehealth.com>
2023-01-26 19:46:10 +0000

    Lastest update at 2023-01-26 19:46:10

--- build.R
+++ build.R
@@ -3,13 +3,13 @@ src_files <-  fs::dir_ls(path = here::here("src"), glob = "*.R")
 
 cmd <- glue::glue("Rscript --vanilla {src_files}")
 
-for(i in seq_along(cmd)){
-    print(cmd[i])
-    tmp <- system(cmd[i], intern=TRUE)
-    if(!is.null(attributes(tmp))){
-    	stop(glue::glue('Non-null attributes returned by command, script may have failed for cmd {cmd[i]}'))
-    }
-}
+#for(i in seq_along(cmd)){
+#    print(cmd[i])
+#    tmp <- system(cmd[i], intern=TRUE)
+#    if(!is.null(attributes(tmp))){
+#    	stop(glue::glue('Non-null attributes returned by command, script may have failed for cmd {cmd[i]}'))
+#    }
+#}
 
 system("git add --all")
 msg = glue::glue("Lastest update at {Sys.time()}")
