088480425a99f8dd9b3b89482fa47fe93f701da1
root <root@datascience2.hs1.mchs.ad>
2023-01-26 19:42:04 +0000

    Update build.R so that errors bubble up to Airflow

--- build.R
+++ build.R
@@ -5,7 +5,10 @@ cmd <- glue::glue("Rscript --vanilla {src_files}")
 
 for(i in seq_along(cmd)){
     print(cmd[i])
-    system(cmd[i])
+    tmp <- system(cmd[i], intern=TRUE)
+    if(!is.null(attributes(tmp))){
+    	stop(glue::glue('Non-null attributes returned by command, script may have failed for cmd {cmd[i]}'))
+    }
 }
 
 system("git add --all")
